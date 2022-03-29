6a9b2032af781c338ee282f9bc024a40dfb92691
pal2509 <paulocapa55@gmail.com>
2022-03-29 21:53:16 +0100

    added caching in movieService

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import axios from 'axios';
+import { PassThrough } from 'stream';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Injectable()
 export class MoviesService {
+  private readonly logger = new Logger(MoviesService.name);
   OMBb_key = '9ed2556f';
   url = `https://www.omdbapi.com/?apikey=${this.OMBb_key}&s=`;
+
+  moviesCache = [];
 
   create(createMovieDto: CreateMovieDto) {
     return 'This action adds a new movie';
@@ -17,7 +21,15 @@ export class MoviesService {
   }
 
   async findOne(name: string) {
-    return axios(this.url + name).then((response) => response.data);
+    if (this.moviesCache.find((x) => x.search === name)) {
+      this.logger.log('cache');
+      return this.moviesCache.find((x) => x.search === name);
+    } else {
+      this.logger.log('request');
+      var response = await axios(this.url + name);
+      this.moviesCache.push({ search: name, data: response.data });
+      return response.data;
+    }
   }
 
   update(id: number, updateMovieDto: UpdateMovieDto) {
